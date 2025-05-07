8d47a651f50a22a2965e1ebf4570916c943c6430
Julian Tatsch <julian@tatsch.it>
2025-05-07 16:42:20 +0200

    fix import in tests, set test paths, add a few more tool configs

---
+++ tests/test_example.py
@@ -0,0 +1,39 @@
+from src.gs1grader.grader_api import DataMatrixGradeAPI
+
+
+def test_example():
+    """
+    Example test function to demonstrate the usage of the grading API.
+    This function grades a DataMatrix image using the modulation grader.
+    """
+    # Create a grading API instance
+    grader_api = DataMatrixGradeAPI()
+
+    # Grade an image using the modulation grader
+    grade, explanation = grader_api.grade_datamatrix(
+        image_path="examples/images/datamatrix_2d_8.png",
+        grade_type="modulation",
+        explanation_path="."
+    )
+
+    assert grade is not None, "Grading failed, no grade returned."
+    assert explanation is not None, "Grading failed, no explanation returned."
+
+
+def test_example_reference():
+    """
+    Example test function to demonstrate the usage of the grading API.
+    This function grades a DataMatrix image using the modulation grader.
+    """
+    # Create a grading API instance
+    grader_api = DataMatrixGradeAPI()
+
+    # Grade an image using the modulation grader
+    grade, explanation = grader_api.grade_datamatrix(
+        image_path="examples/images/datamatrix_2d_original.png",
+        grade_type="modulation",
+        explanation_path="."
+    )
+
+    assert grade is not None, "Grading failed, no grade returned."
+    assert explanation is not None, "Grading failed, no explanation returned."
